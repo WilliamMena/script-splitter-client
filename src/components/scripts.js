ece55180602d62af4b7226b2d745fcca63b70dfe
WilliamMena <williammena1991@gmail.com>
2017-12-21 22:26:41 -0500

    Adjusted connection settings

--- src/components/scripts.js
+++ src/components/scripts.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Scripts = (props) =>
   <div>
-    {props.scripts.map((script, index) =>
-      <div key={index}>
+    {props.scripts.map(script =>
+      <div key={script.id}>
         <h4>{script.title}</h4>
         <p>{script.text}</p>
       </div>
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,28 +2,35 @@ import React, { Component } from 'react';
 import './App.css';
 import Scripts from '../components/scripts.js'
 
+const API_URL = process.env.REACT_APP_API_URL
 
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      scripts: []
+      scripts: [],
+      loading: true
     }
   }
 
 
   componentDidMount() {
-    fetch('http://localhost:3001/api/scripts/')
+    fetch(`${API_URL}/scripts`)
       .then(response => response.json())
-      .then(scripts => this.setState({scripts}))
+      .then(scripts => this.setState({scripts, loading: false}))
   }
 
 
   render() {
     return (
       <div className="App">
-        <Scripts scripts={this.state.scripts} />
+        <h1>All Scripts</h1>
+        {this.state.loading ?
+          <h1>Loading!!!</h1>
+          :
+          <Scripts scripts={this.state.scripts} />
+        }
       </div>
     );
   }
