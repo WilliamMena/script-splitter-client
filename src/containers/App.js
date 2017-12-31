17af0ef4d5b7bd56e1abe471d2c243a86e85492e
WilliamMena <williammena1991@gmail.com>
2017-12-31 15:07:39 -0500

    Removed a lot of rendering from index.js and put it into App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
-import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 
+import './App.css';
 import Home from '../components/Home.js'
+import ScriptsList from './scriptsList.js'
+import ScriptShow from './scriptShow.js'
+import NavBar from '../components/NavBar.js'
 
 class App extends Component {
 
@@ -10,7 +14,14 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Home />
+        <Router>
+          <div className="App">
+            <NavBar />
+            <Route exact path="/" component={Home} />
+            <Route path="/scripts/:scriptId" component={ScriptShow}/>
+            <Route exact path="/scripts" component={ScriptsList} />
+          </div>
+        </Router>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store.js'
-import ScriptsList from './containers/scriptsList.js'
-import ScriptShow from './containers/scriptShow.js'
-import NavBar from './components/NavBar.js'
-
 
 ReactDOM.render(
-
     <Provider store={store}>
-      <Router>
-        <div className="App">
-          <NavBar />
-          <Route exact path="/" component={App} />
-          <Route path="/scripts/:scriptId" component={ScriptShow}/>
-          <Route exact path="/scripts" component={ScriptsList} />
-        </div>
-      </Router>
+      <App />
     </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
